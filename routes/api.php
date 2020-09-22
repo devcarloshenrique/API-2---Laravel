efd00b40e2969960b80c4f0a941afa2cc3b9dbd9
devcarloshenrique <devcarloshenrique@gmail.com>
2020-09-22 11:11:45 -0300

    refactor: one-to-many route

--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,5 @@ Route::get('one-to-one', 'OneToOneController@oneToOne');
 Route::get('one-to-one-inverse', 'OneToOneController@oneToOneInverse');
 
 Route::get('one-to-one-insert', 'OneToOneController@oneToOneInsert');
+
+Route::get('one-to-many', 'OneToManyController@oneToOneMany');
