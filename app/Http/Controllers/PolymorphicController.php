f0a91751152377d5932ee7e9739b87093def99db
devcarloshenrique <devcarloshenrique@gmail.com>
2020-09-27 19:27:50 -0300

    refactor: Polymorphic function to list Country, State and City

--- app/Http/Controllers/PolymorphicController.php
+++ app/Http/Controllers/PolymorphicController.php
@@ -12,6 +12,43 @@ class PolymorphicController extends Controller
 {
     public function polymorphic()
     {
+        //  Exibir Comentarios City
+
+
+        $city = City::where('name', 'Fortaleza')->get()->first();
+
+        echo "<br> <b> Cidade: " . $city->name . " </b> <br>";
+
+        $comments = $city->comments()->get();
+
+        foreach ($comments as $comment) {
+            echo "<br> {$comment->description} <br> <hr>";
+        }
+
+
+        //  Exibir Comentarios State
+
+        $state = State::where('name', 'Acre')->get()->first();
+
+        echo "<br> <b> Estado: " . $state->name . " </b> <br>";
+
+        $comments = $state->comments()->get();
+
+        foreach ($comments as $comment) {
+            echo "<br> {$comment->description} <br> <hr>";
+        }
+
+        //  Exibir Comentarios Country
+
+        $country = Country::where('name', 'Brasil')->get()->first();
+
+        echo "<br> <b> País: " . $country->name . " </b> <br>";
+
+        $comments = $country->comments()->get();
+
+        foreach ($comments as $comment) {
+            echo "<br> {$comment->description} <br> <hr>";
+        }
     }
 
     public function polymorphicInsert()
